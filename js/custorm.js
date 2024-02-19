c48b6f739ac47b6b4b073efb5ebcb9a0abc64634
Modabbir Mossen <125083879+w3modabbir@users.noreply.github.com>
2024-02-19 14:08:33 +0600

    html, css, js, design done

---
+++ js/custorm.js
@@ -0,0 +1,5 @@
+let optionBtn = document.querySelectorAll(".option_btn");
+let advanceBtnOption = document.querySelectorAll(".adv_option");
+let fontNmae = document.querySelector("#fontname")
+let fontSize = document.querySelector("#font_size")
+let input = document.querySelector(".input_text")
